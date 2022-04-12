f7d6977c10b9bf3c4930af82b71e551161f58fea
Lyova Parsyan <parsyan.1994@mail.ru>
2022-04-12 17:06:24 +0400

    :wq

--- carmarket.py
+++ carmarket.py
@@ -6,7 +6,7 @@ import time
 
 class CarMarket:
     def __init__(self):
-        self.cars = {}
+        self.cars = {'Ashot': {'BMW': 3000, 'Nissan': 4000, 'Mercedes': 4000}}
 
     def add_car(self, seller_name):
         if isinstance(seller_name, Seller):
@@ -24,12 +24,16 @@ class CarMarket:
         except KeyError as e:
             print(e, "Impossible remove car from Carmarket")
 
-    def set_discount(self, seller_name, car_name):
-        if car_name in self.cars[f'{seller_name}']:
-            print("Asking Seller for discount...")
-            new_price = seller_name._check_discount(car_name=car_name)
-            self.cars[f'{seller_name}'][car_name] = new_price
-            return (self.cars)
+    def _set_discount(self, seller_name, car_name):
+        if bool(self.cars):
+            if car_name in self.cars[f'{seller_name}']:
+                print("Asking Seller for discount...")
+                new_price = seller_name._check_discount(car_name=car_name)
+                self.cars[f'{seller_name}'][car_name] = new_price
+                return self.cars
+        elif not bool(self.cars):
+            print("Empty list")
+
 
     def _get_sold_car_history(self):
         pass
@@ -47,3 +51,4 @@ car_market = CarMarket()
 # # print(car_market.add_car(ashot))
 # car_market.add_car(ashot)
 # print(car_market.cars)
+print(car_market._set_discount(ashot, "Nissan"))
--- testing.py
+++ testing.py
@@ -1,28 +1,20 @@
 from seller import Seller
 from seller import ashot
 
+a = {'Ashot': {'BMW': 3000, 'Nissan': 4000, 'Mercedes': 4000}}
+
 
 class CarMarket:
     def __init__(self):
-        self.cars = {'Ashot': {'BMW': 3000, 'Nissan': 4000, 'Mercedes': 4000}}
-
-    def remove_car(self, seller_name, car_name):
-        try:
-            if car_name in self.cars[f'{seller_name}']:
-                del self.cars[f'{seller_name}'][car_name]
-                return f"{seller_name}'s {car_name}  was removed from Carmarket"
-            raise KeyError
-        except KeyError as e:
-            print(e, "Impossible remove car from Carmarket")
+        self.cars = {}
 
     def set_discount(self, seller_name, car_name):
-        if car_name in self.cars[f'{seller_name}']:
-            print("Asking Seller for discount...")
-            new_price = seller_name._check_discount(car_name=car_name)
-            self.cars[f'{seller_name}'][car_name] = new_price
-            return (self.cars)
-
+        if bool(self.cars):
+            return True
+        elif not bool(self.cars):
+            return False
 
 
 car_market = CarMarket()
+
 print(car_market.set_discount(ashot, "BMW"))
